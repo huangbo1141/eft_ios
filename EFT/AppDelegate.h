1d26c348da27a2f89c24af4ba77710d56cd91a97
Twinklestar <Twinklestar@twinklestars-macbook-pro.local>
2015-12-07 15:22:28 +0800

    Initial Commit

---
+++ EFT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EFT
+//
+//  Created by Twinklestar on 12/7/15.
+//  Copyright © 2015 Twinklestar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
